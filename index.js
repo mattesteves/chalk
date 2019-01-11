5ac04ae3e7d62c526ad43150c7e91640a0cf5f61
pigbog <mesteves@gmail.com>
2019-01-11 17:09:10 +0000

    messed around with chalk

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var chalk = require("chalk");
 
 var message = "Hello " + chalk.yellow("World");
-console.log(message);
+
+
+console.log(message);	
+
+console.log(chalk.bold(chalk.green(('SUP NERDS!!!'))));
+
+console.log(chalk.italic(chalk.bgMagenta(chalk.green('WHAT ABOUT THIS?'))))
